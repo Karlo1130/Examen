87f3d1a0208cf3a18cf41cd8f9cd4ba3698f7f0d
karlo <kaes_21@@alu.uabcs.mx>
2023-03-24 17:53:20 -0700

    Creacion de la tabla con deteccion del boton seleccionado

--- src/ListaDeUsuarios.java
+++ src/ListaDeUsuarios.java
@@ -1,6 +1,8 @@
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -9,7 +11,7 @@ import java.io.IOException;
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
 
-public class ListaDeUsuarios extends JTable{
+public class ListaDeUsuarios extends JTable implements MouseListener{
 
 	private String[] columns = {"Usuario", "Nombre", "Acciones"};
 	private Object[][] data;
@@ -19,6 +21,7 @@ public class ListaDeUsuarios extends JTable{
 	public ListaDeUsuarios() {
 
 		this.setDefaultRenderer(Object.class, new ButtonRender());
+		this.addMouseListener(this);
 		
 		try {
 			BufferedReader reader = new BufferedReader(new FileReader("users.txt"));
@@ -52,28 +55,28 @@ public class ListaDeUsuarios extends JTable{
 		} 
 		
 		this.setModel(table);	
-
-		button.addActionListener(new ActionListener() {
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
-				/*
-				int column = getColumnModel().getSelectedColumnCount(); 
-				int row = getRowModel().getSelectedRowCount();
-
-				if(row < getRowCount() && row >= 0 && column < getColumnCount() && column >= 0) {
-					Object value = getValueAt(row, column);
-					if(value instanceof JButton) {
-						((JButton)value).doClick();
-						JButton boton = (JButton) value;
-						System.out.println("lol");
-					}
-				}
-				*/
-			}
-			
-		});
+		System.out.println("si");
+//		button.addActionListener(new ActionListener() {
+//
+//			@Override
+//			public void actionPerformed(ActionEvent e) {
+//				// TODO Auto-generated method stub
+//				/*
+//				int column = getColumnModel().getSelectedColumnCount(); 
+//				int row = getRowHeight();
+//
+//				/*if(row < getRowCount() && row >= 0 && column < getColumnCount() && column >= 0) {*/
+//					Object value = getValueAt(row, column);
+//					if(value instanceof JButton) {
+//						((JButton)value).doClick();
+//						JButton boton = (JButton) value;
+//						System.out.println("lol");
+//					}
+//				/*}
+//				*/
+//			}
+//			
+//		});
 		
 			
 	}
@@ -104,5 +107,47 @@ public class ListaDeUsuarios extends JTable{
 	public boolean isCellEditable(int row, int column) {
 		return false;
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		int column = getColumnModel().getColumnIndexAtX(e.getX()); 
+		int row = e.getY()/getRowHeight();
+		System.out.println(row);
+		/*
+		if(row < getRowCount() && row >= 0 && column < getColumnCount() && column >= 0) {
+			Object value = getValueAt(row, column);
+			if(value instanceof JButton) {
+				((JButton)value).doClick();
+				JButton boton = (JButton) value;
+				
+			}
+		}*/
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
 
--- src/Ventana.java
+++ src/Ventana.java
@@ -1,8 +1,13 @@
 import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
 import javax.swing.*;
 
 public class Ventana extends JFrame{
-
+	
+	ListaDeUsuarios tabla;
+	
 	public Ventana() {
 		
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -12,11 +17,56 @@ public class Ventana extends JFrame{
 		this.setTitle("Gestion de Usuarios");
 		this.setSize(600, 800);
 
-		JScrollPane test = new JScrollPane(new ListaDeUsuarios());
+		tabla = new ListaDeUsuarios();
+		
+		//JScrollPane test = new JScrollPane(new ListaDeUsuarios());
+		JScrollPane test = new JScrollPane(tabla);
 		this.add(test);
 
 		this.repaint();
 		this.revalidate();
 	}
+//
+//	@Override
+//	public void mouseClicked(MouseEvent e) {
+//		// TODO Auto-generated method stub
+//		System.out.println("le");
+//		
+//		int column = tabla.getColumnModel().getColumnIndexAtX(e.getX()); 
+//		int row = e.getY()/tabla.getRowHeight();
+//		
+//		if(row < tabla.getRowCount() && row >= 0 && column < tabla.getColumnCount() && column >= 0) {
+//			Object value = tabla.getValueAt(row, column);
+//			if(value instanceof JButton) {
+//				((JButton)value).doClick();
+//				JButton boton = (JButton) value;
+//				System.out.println("lol");
+//			}
+//		}
+//	}
+//
+//	@Override
+//	public void mousePressed(MouseEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void mouseReleased(MouseEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void mouseEntered(MouseEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
+//
+//	@Override
+//	public void mouseExited(MouseEvent e) {
+//		// TODO Auto-generated method stub
+//		
+//	}
 	
 }
